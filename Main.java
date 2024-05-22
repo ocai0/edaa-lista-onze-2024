91799398c179b6380fa56a6db8239538b6b6b37d
ocai0 <caioalvesfernandes@gmail.com>
2024-05-22 20:26:03 -0300

    uncommented other entries

--- Main.java
+++ Main.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // System.out.println("------------------------------------------------------------");
-        // System.out.println("Exercício Um \n");
-        // ExercicioUm.resolve();
-        // System.out.println("------------------------------------------------------------");
-        // System.out.println("Exercício Dois \n");
-        // ExercicioDois.resolve(scanner);
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Exercício Um \n");
+        ExercicioUm.resolve();
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Exercício Dois \n");
+        ExercicioDois.resolve(scanner);
         System.out.println("------------------------------------------------------------");
         System.out.println("Exercício Três \n");
         ExercicioTres.resolve(scanner);
